a80c7032e3cd61501180560dc819358fd9d915af
Jeff McFadden <jeff@forgeapps.com>
2012-05-23 19:36:45 -0700

    Okay here's a full engine. Let's try this one out.

---
+++ lib/engrave/engine.rb
@@ -0,0 +1,4 @@
+module Engrave
+  class Engine < ::Rails::Engine
+  end
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '03a1c4403aaec240cb7a4819f182fd6c04314ea96a4a476b079c2be4adb049f63b8bb6ba875e4110bf6ce88e112aa484639c091c2c2180a988cedb3b3a486c74'
