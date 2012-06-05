66c957352f6ee6abad0d9321b25fb774f5ea4cb4
Jeff McFadden <jeff@forgeapps.com>
2012-06-05 14:17:18 -0700

    Reorder routes and fix broken published scope in Posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :author
   attr_accessible :body, :draft, :published_at, :slug, :story_link, :title
     
-  scope :published, where( [ "draft = ? AND published_at < ?", false, Time.now ] ).order( 'published_at DESC' )
+  scope :published, lambda { where( [ "draft = ? AND published_at < ?", false, Time.now ] ).order( 'published_at DESC' ) }
+  
   scope :draft, where( :draft => true )
   
   extend ::FriendlyId
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :pages
   resources :posts
+  resources :pages
   resources :authors
 end
